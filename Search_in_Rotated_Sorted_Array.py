53ccf9394482eaf4ccd3a10d5500a61717963db1
YaccharlaDineshChandra <98159705+YaccharlaDineshChandra@users.noreply.github.com>
2023-07-03 13:55:28 +0530

    n=int(input()) l=list(map(int,input().split())) k=int(input()) if k in l:     print(l.index(k)) else:     print('-1')

--- Search_in_Rotated_Sorted_Array.py
+++ Search_in_Rotated_Sorted_Array.py
@@ -1,9 +1,7 @@
 n=int(input())
 l=list(map(int,input().split()))
 k=int(input())
-for i in range(len(l)):
-    if l[i]==k:
-        print(i)
-        break
+if k in l:
+    print(l.index(k))
 else:
     print('-1')
